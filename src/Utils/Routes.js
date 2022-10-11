d1186d45a83bdfd1f65ac2751d144355bd0f70d7
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-10-11 12:45:27 +0600

    added errorpage

--- src/Utils/Routes.js
+++ src/Utils/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog";
 import Body from "../components/Body";
+import ErrorPage from "../components/ErrorPage";
 import Home from "../components/Home";
 
 import Root from "../components/Root";
@@ -10,6 +11,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             { path: '/', element: <Home></Home> },
             { path: '/body', element: <Body></Body> },
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-
+import Students from '../assets/Students.jpg'
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home</h1>
-        </div>
+        <section>
+            <div className="bg-gray-100">
+                <div className="container flex flex-col items-center pb-24 mx-auto text-center lg:pb-56 md:py-30 md:px-10 lg:px-25 text-gray-900">
+                    <h1 className='text-xl'>Wellcome to Learnign website to know something.</h1>
+                    <p>First to show our rules and regulation.</p>
+                </div>
+            </div>
+            <img
+                src={Students}
+                alt=""
+                className="w-3/6 mx-auto mb-12 -mt-20 rounded-lg shadow-md lg:-mt-40 bg-gray-500"
+            />
+
+        </section>
     );
 };
 
