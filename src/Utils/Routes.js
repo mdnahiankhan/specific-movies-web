90b49f16ebadc76f0a991e8d59a8a75a4d41cf87
Md. Nahian <102530128+mdnahiankhan@users.noreply.github.com>
2022-10-11 12:09:56 +0600

    added Header and Home

--- src/Utils/Routes.js
+++ src/Utils/Routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Blog from "../components/Blog";
 import Body from "../components/Body";
+import Home from "../components/Home";
 
 import Root from "../components/Root";
 import Statistics from "../components/Statistics";
@@ -10,9 +11,11 @@ const router = createBrowserRouter([
         path: '/',
         element: <Root></Root>,
         children: [
+            { path: '/', element: <Home></Home> },
             { path: '/body', element: <Body></Body> },
             { path: '/statistics', element: <Statistics></Statistics> },
-            { path: '/blog', element: <Blog></Blog> }
+            { path: '/blog', element: <Blog></Blog> },
+            { path: '/home', element: <Home></Home> }
         ]
 
     }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,28 @@ const Header = () => {
         <div>
             <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
                 <div className="container flex justify-between h-16 mx-auto">
-                    <a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-                        <h1 className='text-3xl'>Question Answering Website</h1>
-                    </a>
+                    <Link rel="noopener noreferrer" aria-label="Back to homepage" className="flex items-center p-2">
+                        <h1 className='text-3xl'>Question Answering Website</h1></Link>
                     <ul className="items-stretch hidden space-x-3 md:flex">
                         <li className="flex">
-                            <Link to='/'>  <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Home</a>
+                            <Link to='/' rel="noopener noreferrer" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Home
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/body'>  <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Topics</a>
+                            <Link to='/body' rel="noopener noreferrer" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">   Topics
                             </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/statistics'> <a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Statistics</a></Link>
+                            <Link to='/statistics'
+                                rel="noopener noreferrer" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Statistics </Link>
                         </li>
                         <li className="flex">
-                            <Link to='/blog'><a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">BLog</a></Link>
+                            <Link to='/blog' rel="noopener noreferrer" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">BLog</Link>
                         </li>
                     </ul>
                 </div>
-            </header>
-        </div>
+            </header >
+        </div >
     );
 };
 
